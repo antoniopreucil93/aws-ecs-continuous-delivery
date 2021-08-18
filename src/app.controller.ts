296be584903cf610d24abf6ef6b887cb2e15672d
antonio.preucil <antonio.preucil@dings.solutions>
2021-08-18 21:46:15 +0200

    final

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import * as AWS from 'aws-sdk';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +7,23 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello() {
+    const region = 'eu-central-1';
+    const secretName = 'ENV_TEST_APP';
+
+    // Create a Secrets Manager client
+    const client = new AWS.SecretsManager({
+      region: region,
+    });
+
+    const { SecretString } = await client
+      .getSecretValue({ SecretId: secretName })
+      .promise();
+
+    const secret = JSON.parse(SecretString);
+
+    console.log(process.env.GITHUB_TOKEN, ' data');
+
+    return secret['FIRST_ENV'];
   }
 }
