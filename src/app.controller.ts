14c3a17158042c9359e9774b9f2b34ebd3fa7de5
antonio.preucil <antonio.preucil@dings.solutions>
2021-08-24 19:41:03 +0200

    fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,22 +6,6 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  async getHello() {
-    // const region = 'eu-central-1';
-    // const secretName = 'ENV_TEST_APP';
-    // // Create a Secrets Manager client
-    // const client = new AWS.SecretsManager({
-    //   region: region,
-    // });
-    // const { SecretString } = await client
-    //   .getSecretValue({ SecretId: secretName })
-    //   .promise();
-    // const secret = JSON.parse(SecretString);
-    // console.log(process.env.GITHUB_TOKEN, ' data');
-    // return secret['FIRST_ENV'];
-  }
-
   @Get('hello')
   getHelloWorld() {
     return 'hello world';
